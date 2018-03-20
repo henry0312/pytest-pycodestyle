a08a7e484bc29882d5448bd5fbc6133db911d491
Tsukasa OMOTO <henry0312@gmail.com>
2018-03-20 13:27:15 +0900

    Also use "pytest-pycodestyle" as package name (#16)
    https://github.com/henry0312/pytest-codestyle/issues/15

--- setup.py
+++ setup.py
@@ -1,36 +1,44 @@
+import os
+
 from setuptools import setup
 
-setup(
-    name='pytest-codestyle',
-    version='1.1.2',
-    description='pytest plugin to run pycodestyle',
-    url='https://github.com/henry0312/pytest-codestyle',
-    author='Tsukasa OMOTO',
-    author_email='henry0312@gmail.com',
-    license='MIT',
-    py_modules=['pytest_codestyle'],
-    python_requires='>=3.6,<4',
-    install_requires=[
-        'pytest>=3.0,<4',
-        'py>=1.5,<1.6',
-        'pycodestyle>=2.3,<2.4',
-    ],
-    extras_require={
-        'tests': [
-            'pytest-isort>=0.1,<1.0',
+if os.environ.get('BUILD', None):
+    packages = ['pytest-codestyle', 'pytest-pycodestyle']
+else:
+    packages = ['pytest-codestyle']
+
+for name in packages:
+    setup(
+        name=name,
+        version='1.1.2',
+        description='pytest plugin to run pycodestyle',
+        url='https://github.com/henry0312/pytest-codestyle',
+        author='Tsukasa OMOTO',
+        author_email='henry0312@gmail.com',
+        license='MIT',
+        py_modules=['pytest_codestyle'],
+        python_requires='>=3.6,<4',
+        install_requires=[
+            'pytest>=3.0,<4',
+            'py>=1.5,<1.6',
+            'pycodestyle>=2.3,<2.4',
         ],
-    },
-    # https://docs.pytest.org/en/latest/writing_plugins.html#making-your-plugin-installable-by-others
-    entry_points={
-        'pytest11': [
-            'codestyle = pytest_codestyle',
-        ]
-    },
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Testing',
-        'License :: OSI Approved :: MIT License',
-        'Framework :: Pytest',
-    ],
-)
+        extras_require={
+            'tests': [
+                'pytest-isort>=0.1,<1.0',
+            ],
+        },
+        # https://docs.pytest.org/en/latest/writing_plugins.html#making-your-plugin-installable-by-others
+        entry_points={
+            'pytest11': [
+                'codestyle = pytest_codestyle',
+            ]
+        },
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Testing',
+            'License :: OSI Approved :: MIT License',
+            'Framework :: Pytest',
+        ],
+    )
