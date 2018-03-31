fb43d27f7f67bd851bdfc8958fabfd8261cc32d0
Tsukasa OMOTO <tomoto@if-el.se>
2018-03-31 15:24:11 +0900

    Fix and update setup.py (#22)
    * Fix setup.py
    
    * Fix and update setup.py
    
    - revert a08a7e484bc29882d5448bd5fbc6133db911d491
    - load VERSION
    - load README.md
    
    https://github.com/henry0312/pytest-codestyle/issues/21

--- setup.py
+++ setup.py
@@ -2,46 +2,44 @@ import os
 
 from setuptools import setup
 
-if os.environ.get('BUILD', None):
-    packages = ['pytest-codestyle', 'pytest-pycodestyle']
-else:
-    packages = ['pytest-codestyle']
+version = open('VERSION').read().strip()
+long_description = open('README.md').read()
 
-for name in packages:
-    setup(
-        name=name,
-        version='1.2.0',
-        description='pytest plugin to run pycodestyle',
-        url='https://github.com/henry0312/pytest-codestyle',
-        author='Tsukasa OMOTO',
-        author_email='henry0312@gmail.com',
-        license='MIT',
-        py_modules=['pytest_codestyle'],
-        python_requires='~=3.5',
-        install_requires=[
-            'pytest>=3.0,<4',
-            'py>=1.5,<1.6',
-            'pycodestyle>=2.3,<2.4',
+setup(
+    name='pytest-codestyle',
+    version=version,
+    description='pytest plugin to run pycodestyle',
+    long_description=long_description,
+    url='https://github.com/henry0312/pytest-codestyle',
+    author='Tsukasa OMOTO',
+    author_email='henry0312@gmail.com',
+    license='MIT',
+    py_modules=['pytest_codestyle'],
+    python_requires='~=3.5',
+    install_requires=[
+        'pytest>=3.0,<4',
+        'py>=1.5,<1.6',
+        'pycodestyle>=2.3,<2.4',
+    ],
+    extras_require={
+        'tests': [
+            'pytest-isort>=0.1,<1.0',
         ],
-        extras_require={
-            'tests': [
-                'pytest-isort>=0.1,<1.0',
-            ],
-        },
-        # https://docs.pytest.org/en/latest/writing_plugins.html#making-your-plugin-installable-by-others
-        entry_points={
-            'pytest11': [
-                'codestyle = pytest_codestyle',
-            ]
-        },
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Topic :: Software Development :: Testing',
-            'License :: OSI Approved :: MIT License',
-            'Framework :: Pytest',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-        ],
-    )
+    },
+    # https://docs.pytest.org/en/latest/writing_plugins.html#making-your-plugin-installable-by-others
+    entry_points={
+        'pytest11': [
+            'codestyle = pytest_codestyle',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Pytest',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
