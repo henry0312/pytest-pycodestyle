6d5f17364f08355670169ae52af26247cc6b124e
OMOTO Tsukasa <tsukasa@oomo.to>
2020-03-23 00:54:05 +0900

    Release v2.1.0 (#69)

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Framework :: Pytest',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
